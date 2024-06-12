bf48f3f3e3cf903e024b82010489f8fbdd3212ad
paulo <pgabrieldeveloper@outlook.com>
2024-06-12 08:02:42 -0300

    feat(cors): add cors

---
+++ src/main/java/com/pgabrieldeveloper/music_adjaf_mf3/music_adjaf_mf3/config/CorsFilter.java
@@ -0,0 +1,30 @@
+package com.pgabrieldeveloper.music_adjaf_mf3.music_adjaf_mf3.config;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+
+@Configuration
+public class CorsFilter {
+
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*")
+                        .allowedHeaders(HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION);
+            }
+        };
+    }
+}
---
+++ src/main/java/com/pgabrieldeveloper/music_adjaf_mf3/music_adjaf_mf3/music/model/RequestMusicPressinedURL.java
@@ -0,0 +1,9 @@
+package com.pgabrieldeveloper.music_adjaf_mf3.music_adjaf_mf3.music.model;
+
+
+import java.util.List;
+
+public record RequestMusicPressinedURL(
+        List<String> musicsIds
+) {
+}
