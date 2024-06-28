5a72a3ed0eee68830cd9bdef18ee76423d025ba0
paulo <pgabrieldeveloper@outlook.com>
2024-06-28 18:00:44 -0300

    refactor(S3): rmeovendo region

--- src/main/java/com/pgabrieldeveloper/music_adjaf_mf3/music_adjaf_mf3/config/S3Config.java
+++ src/main/java/com/pgabrieldeveloper/music_adjaf_mf3/music_adjaf_mf3/config/S3Config.java
@@ -2,6 +2,7 @@ package com.pgabrieldeveloper.music_adjaf_mf3.music_adjaf_mf3.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,17 +19,13 @@ public class S3Config {
     @Value("${SECRET_KEY}")
     private String secretKey;
 
-    @Value("${AWS_REGION}")
-    private String region;
-
-
     @Bean
     public AmazonS3 s3Client() {
         var credentials = new BasicAWSCredentials(accessKey,secretKey);
         var S3Client = AmazonS3Client
                 .builder()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
+                .withRegion(Regions.US_EAST_2)
                 .build();
 
         return S3Client;
